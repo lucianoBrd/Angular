62a1c0a77d7e409c77b1c3dd8045d57fb61eaa55
lucianoBrd <lucien.burdet@gmail.com>
2019-03-06 09:26:12 +0100

    paul

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,13 +2,28 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
+  { id: 12, name: 'Paul' },
   { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 14, name: 'Paulette' },
+  { id: 15, name: 'Paulo' },
+  { id: 16, name: 'Paulito' },
+  { id: 17, name: 'Jean-Paul' },
+  { id: 18, name: 'Paul Dance' },
+  { id: 19, name: 'Dr Paul' },
+  { id: 20, name: 'Paulinome' },
+  { id: 21, name: 'Paul Ochon' },
+  { id: 22, name: 'Paul Nord' },
+  { id: 23, name: 'Paul Isson' },
+  { id: 24, name: 'Paul Itique' },
+  { id: 25, name: 'Paul Ynésie' },
+  { id: 26, name: 'Paultergeist' },
+  { id: 27, name: 'Paul Yakov' },
+  { id: 28, name: 'Paultron' },
+  { id: 29, name: 'Paul Emploi' },
+  { id: 30, name: 'Paulaine' },
+  { id: 31, name: 'Paul Lapute' },
+  
+  
+  
+  
 ];
