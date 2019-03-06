b77dce01d20b7ab8ed6d611f790a2a32f2e607ba
lucianoBrd <lucien.burdet@gmail.com>
2019-03-06 09:15:16 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
-import { MessagesComponent } from './messages/messages.component'; // <-- NgModel lives here
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
